cee01fb56609393fe39bb72e1ed4892157c80574
Jose <jmejiamu@gmail.com>
2021-01-24 20:39:38 -0800

    Working on the request method

--- src/App.js
+++ src/App.js
@@ -1,58 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const [data, setData] = useState([]);
+
+    const getData = async () => {
+        try {
+            const response = await fetch('http://localhost:8080/womenitems'
+            );
+            const resJson = await response.json();
+            console.log('>>>>>>>', resJson);
+            setData(resJson);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        getData();
+    }, [])
+    return (
+        <div className="App">
+            <h1>Market Item</h1>
+            {
+                data.map((items, index) => {
+                    return (
+                        <div key={index}>
+                            <p>{items.itemName}</p>
+                        </div>
+                    );
+                })
+            }
+        </div>
+    );
 }
 
 export default App;
