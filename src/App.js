533ab2d106740def9de4d8d2f387a6af56e353c8
Jose <jmejiamu@gmail.com>
2021-02-21 00:18:27 -0800

    added home screen component

--- src/App.js
+++ src/App.js
@@ -6,43 +6,45 @@ import { fetchData, itemsSelector } from './features/counter/fetchSlice';
 import './App.css';
 
 import '../src/components/styles/styles.scss';
+import Home from './components/Home/Home';
 
 const App = () => {
 
-    const dispatch = useDispatch()
-    const { info, loading, hasErrors } = useSelector(itemsSelector)
+    // const dispatch = useDispatch()
+    // const { info, loading, hasErrors } = useSelector(itemsSelector)
 
-    console.log('INFO: ', info);
+    // console.log('INFO: ', info);
 
-    useEffect(() => {
-        dispatch(fetchData())
-    }, [dispatch])
+    // useEffect(() => {
+    //     dispatch(fetchData())
+    // }, [dispatch])
 
     // error handling & map successful query data 
-    const renderData = () => {
-        if (loading) return <p>Loading recipes...</p>
-        if (hasErrors) return <p>Cannot display recipes...</p>
+    // const renderData = () => {
+    //     if (loading) return <p>Loading recipes...</p>
+    //     if (hasErrors) return <p>Cannot display recipes...</p>
 
-        return info.map((item, index) =>
-            <div key={index} className='card card-style mt-5 grid-item mb-5'>
-                <img className="card-img-top" src={item.itemUrl} alt="women" />
-                <div className="card-body" >
-                    <h5 className="card-title">{item.itemName}</h5>
-                    <p className="card-text" >{item.itemDescrition}</p>
-                </div>
-            </div>
+    //     return info.map((item, index) =>
+    //         <div key={index} className='card card-style mt-5 grid-item mb-5'>
+    //             <img className="card-img-top" src={item.itemUrl} alt="women" />
+    //             <div className="card-body" >
+    //                 <h5 className="card-title">{item.itemName}</h5>
+    //                 <p className="card-text" >{item.itemDescrition}</p>
+    //             </div>
+    //         </div>
 
 
-        )
-    }
+    //     )
+    // }
 
     return (
         <div className="App">
             <h1>Market Item</h1>
-            <div className="grid-container">
+            {/* <div className="grid-container">
 
                 {renderData()}
-            </div>
+            </div> */}
+            <Home />
 
         </div>
     );
