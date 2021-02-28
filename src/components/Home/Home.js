9f852190b93b5e45ecde58ca7893d9fc4db41351
Jose <jmejiamu@gmail.com>
2021-02-28 14:13:06 -0800

    added popular section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,6 +6,7 @@ import { fetchData, itemsSelector } from '../../features/counter/fetchSlice';
 import Carousel from '../Carousel/Carousel';
 import Category from '../Category/Category';
 import NavBar from '../NavBar/NavBar';
+import New from '../New/New';
 
 const Home = () => {
 
@@ -44,36 +45,34 @@ const Home = () => {
                 {renderData()}
             </div> */}
             <Category />
+            <New />
             <div className="container" >
-                <h2 className="new-title">New</h2>
+                <h2 className="popular-title">Popular</h2>
                 <div className="grid-container" >
 
                     <div className='card card-style mt-5 grid-item mb-5'>
-                        <img className="card-img-top img-style" src="https://images.unsplash.com/photo-1516257984-b1b4d707412e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="men" />
+                        <img className="card-img-top img-style-popular" src="https://images.unsplash.com/photo-1563293958-0159cd7dd208?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=648&q=80" alt="men" />
                         <div className="card-body" >
                             <h5 className="card-title">Mens Jacket</h5>
                             <p className="card-text" >Description</p>
                         </div>
                     </div>
                     <div className='card card-style mt-5 grid-item mb-5'>
-                        <img className="card-img-top img-style" src="https://images.unsplash.com/photo-1520903214476-57060aabb71d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="men" />
+                        <img className="card-img-top img-style-popular" src="https://images.unsplash.com/photo-1560528238-73adf75ca6da?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="men" />
                         <div className="card-body" >
                             <h5 className="card-title">Mens Jacket</h5>
                             <p className="card-text" >Description</p>
                         </div>
                     </div>
                     <div className='card card-style mt-5 grid-item mb-5'>
-                        <img className="card-img-top img-style" src="https://images.unsplash.com/photo-1487222477894-8943e31ef7b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=726&q=80" alt="men" />
+                        <img className="card-img-top img-style-popular" src="https://images.unsplash.com/photo-1611312449408-fcece27cdbb7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=618&q=80" alt="men" />
                         <div className="card-body" >
                             <h5 className="card-title">Mens Jacket</h5>
                             <p className="card-text" >Description</p>
                         </div>
                     </div>
                 </div>
-
-
             </div>
-            <h2>Popular</h2>
         </>
     );
 };
---
+++ src/components/New/New.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const New = () => {
+    return (
+        <div className="container" >
+            <h2 className="new-title">New</h2>
+            <div className="grid-container" >
+
+                <div className='card card-style mt-5 grid-item mb-5'>
+                    <img className="card-img-top img-style" src="https://images.unsplash.com/photo-1516257984-b1b4d707412e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="men" />
+                    <div className="card-body" >
+                        <h5 className="card-title">Mens Jacket</h5>
+                        <p className="card-text" >Description</p>
+                    </div>
+                </div>
+                <div className='card card-style mt-5 grid-item mb-5'>
+                    <img className="card-img-top img-style" src="https://images.unsplash.com/photo-1520903214476-57060aabb71d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="men" />
+                    <div className="card-body" >
+                        <h5 className="card-title">Mens Jacket</h5>
+                        <p className="card-text" >Description</p>
+                    </div>
+                </div>
+                <div className='card card-style mt-5 grid-item mb-5'>
+                    <img className="card-img-top img-style" src="https://images.unsplash.com/photo-1487222477894-8943e31ef7b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=726&q=80" alt="men" />
+                    <div className="card-body" >
+                        <h5 className="card-title">Mens Jacket</h5>
+                        <p className="card-text" >Description</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default New;
