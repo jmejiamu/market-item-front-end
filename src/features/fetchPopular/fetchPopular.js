cd5477879cf5540e2b34a0c25c477c5671958110
Jose <jmejiamu@gmail.com>
2021-03-16 15:17:08 -0700

    added slice to fetch popular data

---
+++ src/features/fetchPopular/fetchPopular.js
@@ -0,0 +1,51 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const popularDataSlice = createSlice({
+    name: 'popular',
+    initialState: {
+        loading: false,
+        hasError: false,
+        popular: []
+    },
+
+    reducers: {
+        getData: state => {
+            state.loading = true;
+        },
+        getDataSuccess: (state, action) => {
+            state.popular = action.payload
+            state.loading = false
+            state.hasError = false
+        },
+        getDataFailure: state => {
+            state.loading = false
+            state.hasError = true
+        }
+    }
+})
+
+// three action generated from the slice
+export const { getData, getDataSuccess, getDataFailure } = popularDataSlice.actions
+
+
+// Selector
+export const popularSelector = state => state.popular
+
+// The reducer 
+export default popularDataSlice.reducer
+
+// Asynchronous thunk action
+export function fetchData() {
+    return async dispatch => {
+        dispatch(getData())
+
+        try {
+            const response = await fetchData('http://localhost:8080/api/news');
+            const data = await response.json();
+
+            dispatch(getDataSuccess(data._embedded.news))
+        } catch (error) {
+            dispatch(getDataFailure());
+        }
+    }
+}
