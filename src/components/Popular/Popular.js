fa05f90d09d7624d3f6d3c2d16f71366f3624b15
Jose <jmejiamu@gmail.com>
2021-03-16 17:10:44 -0700

    added redux fetch for popular section

--- src/components/Popular/Popular.js
+++ src/components/Popular/Popular.js
@@ -9,7 +9,24 @@ const Popular = () => {
     const dispatch = useDispatch();
     const { popular, loading, hasError } = useSelector(popularSelector);
 
-    // console.log('>>HEre', popular);
+    // Error handling & map successful query data
+    const renderData = () => {
+        if (loading) return <p>Loading ...</p>
+        if (hasError) return <p>Something wrong may happends</p>
+
+        return popular.map((item, index) => {
+            return (
+                <div key={index} className='card card-style mt-5 grid-item mb-5'>
+                    <img className="card-img-top img-style-popular" src={item.itemUrl} alt="men" />
+                    <div className="card-body" >
+                        <h5 className="card-title">{item.itemName}</h5>
+                        <p className="card-text" >{item.description}</p>
+                    </div>
+                </div>
+            )
+        })
+
+    }
 
     useEffect(() => {
         dispatch(fetchData());
@@ -18,28 +35,8 @@ const Popular = () => {
         <div className="container" >
             <h2 className="popular-title">Popular</h2>
             <div className="grid-container" >
+                {renderData()}
 
-                <div className='card card-style mt-5 grid-item mb-5'>
-                    <img className="card-img-top img-style-popular" src="https://images.unsplash.com/photo-1563293958-0159cd7dd208?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=648&q=80" alt="men" />
-                    <div className="card-body" >
-                        <h5 className="card-title">Women</h5>
-                        <p className="card-text" >Description</p>
-                    </div>
-                </div>
-                <div className='card card-style mt-5 grid-item mb-5'>
-                    <img className="card-img-top img-style-popular" src="https://images.unsplash.com/photo-1560528238-73adf75ca6da?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="men" />
-                    <div className="card-body" >
-                        <h5 className="card-title">Women</h5>
-                        <p className="card-text" >Description</p>
-                    </div>
-                </div>
-                <div className='card card-style mt-5 grid-item mb-5'>
-                    <img className="card-img-top img-style-popular" src="https://images.unsplash.com/photo-1611312449408-fcece27cdbb7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=618&q=80" alt="men" />
-                    <div className="card-body" >
-                        <h5 className="card-title">Mens Jacket</h5>
-                        <p className="card-text" >Description</p>
-                    </div>
-                </div>
             </div>
         </div>
     );
