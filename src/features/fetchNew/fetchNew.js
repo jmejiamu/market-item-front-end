dd92b16f0fb0dfd2681c7e1fda3b9bb79e51029e
Jose <jmejiamu@gmail.com>
2021-03-17 19:19:10 -0700

    added new slice redux

---
+++ src/features/fetchNew/fetchNew.js
@@ -0,0 +1,49 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const newDataSlice = createSlice({
+    name: 'new',
+    initialState: {
+        loading: false,
+        hasError: false,
+        new: []
+    },
+    reducers: {
+        getData: state => {
+            state.loading = true;
+        },
+        getDataSucess: (state, action) => {
+            state.new = action.payload
+            state.loading = false
+            state.hasError = false
+        },
+        getDataFailure: state => {
+            state.loading = false
+            state.hasError = true
+        }
+    }
+})
+
+// thre action generated from the slice
+export const { getData, getDataSucess, getDataFailure } = newDataSlice.actions
+
+// seletor
+export const newSelector = state => state.new
+
+//  The reducer
+export default newDataSlice.reducer
+
+// Asynchronous thunk action
+export function fetchData() {
+    return async dispatch => {
+        dispatch(getData())
+
+        try {
+            const response = await fetch('http://localhost:8080/api/news');
+            const data = await response.json();
+
+            dispatch(getDataSucess(data._embedded.news));
+        } catch (error) {
+            dispatch(getDataFailure());
+        }
+    }
+}
