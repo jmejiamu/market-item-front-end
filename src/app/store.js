c55c3aa7b222684aa4785f384acd7102c915f65a
Jose <jmejiamu@gmail.com>
2021-03-17 19:26:17 -0700

    added new reducer to the store

--- src/app/store.js
+++ src/app/store.js
@@ -3,12 +3,14 @@ import counterReducer from '../features/counter/counterSlice';
 import infoReducer from '../features/counter/fetchSlice';
 import carouselReducer from '../features/fetchCarousel/fetchCarousel';
 import popularReducer from '../features/fetchPopular/fetchPopular';
+import newReducer from '../features/fetchNew/fetchNew';
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
     info: infoReducer,
     carousel: carouselReducer,
-    popular: popularReducer
+    popular: popularReducer,
+    new: newReducer
   },
 });
