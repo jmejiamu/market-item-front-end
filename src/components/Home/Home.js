3a7481a102f4c6526029bb4b5d574b16ba824f58
Jose <jmejiamu@gmail.com>
2021-03-19 23:37:26 -0700

    removing unnessesary code

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,6 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
-import { useDispatch, useSelector } from 'react-redux';
 
-import { fetchData, itemsSelector } from '../../features/counter/fetchSlice';
 
 import Carousel from '../Carousel/Carousel';
 import Category from '../Category/Category';
@@ -12,32 +10,6 @@ import Popular from '../Popular/Popular';
 
 const Home = () => {
 
-    const dispatch = useDispatch();
-
-    const { info, loading, hasError } = useSelector(itemsSelector);
-
-    useEffect(() => {
-        dispatch(fetchData());
-    }, [dispatch])
-
-
-    // error handling & map successful query data 
-    const renderData = () => {
-        if (loading) return <p>Loading ...</p>
-        if (hasError) return <p>An error has ocurrer ...</p>
-
-        return info.map((item, index) => {
-            return (
-                <div key={index} className='card card-style mt-5 grid-item mb-5'>
-                    <img className="card-img-top" src={item.itemUrl} alt="women" />
-                    <div className="card-body" >
-                        <h5 className="card-title">{item.itemName}</h5>
-                        <p className="card-text" >{item.itemDescrition}</p>
-                    </div>
-                </div>
-            )
-        })
-    }
 
     return (
         <>
