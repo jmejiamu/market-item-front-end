58676181600ebe39819db62b6eece29d9bf8f306
Jose <jmejiamu@gmail.com>
2021-03-22 01:12:46 -0700

    added nav to the mens  items section

--- src/components/Men/MenItems.js
+++ src/components/Men/MenItems.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import NavBar from '../NavBar/NavBar';
 
 const MenItems = () => {
     return (
         <div>
+            <NavBar />
+
             <h1>This is mens Items </h1>
+            <h2>the</h2>
         </div>
     );
 };
