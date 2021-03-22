02c7bc7af2a9057452eb0e677b5b2039027cbf3f
Jose <jmejiamu@gmail.com>
2021-03-21 23:56:58 -0700

    configuring routes

--- src/App.js
+++ src/App.js
@@ -5,16 +5,24 @@ import './App.css';
 
 import '../src/components/styles/styles.scss';
 import Home from './components/Home/Home';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import MenItems from './components/Men/MenItems';
 
 const App = () => {
 
 
     return (
-        <div className="App">
+        <BrowserRouter>
+            <div className="App">
+                <Switch>
+                    <Route exact={true} path='/' render={props => <Home {...props} />} />
+                    <Route exact={true} path='/mens' render={props => <MenItems {...props} />} />
+                    {/* <Route exact path='/mens' component={MenItems} /> */}
+                </Switch>
 
-            <Home />
 
-        </div>
+            </div>
+        </BrowserRouter>
     );
 }
 
