42e8a8e05a49315d8b2f33c5dbd8d78fa51592d8
Jose <jmejiamu@gmail.com>
2021-03-22 00:07:15 -0700

    added link to the mens items

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Category = () => {
     return (
@@ -8,10 +9,12 @@ const Category = () => {
             <div className="wrapper">
                 <div class="row">
                     <div class="column">
-                        <img className="img-category" src="https://images.unsplash.com/photo-1488161628813-04466f872be2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80" alt="Snow" style={{ width: '100 %' }} />
+                        <Link to='/mens'>
+                            <img className="img-category" src="https://images.unsplash.com/photo-1488161628813-04466f872be2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80" alt="Snow" style={{ width: '100 %' }} />
 
-                        <div className="text" >Men</div>
+                            <div className="text" >Men</div>
 
+                        </Link>
                     </div>
                     <div class="column">
                         <img className="img-category" src="https://images.unsplash.com/photo-1494354145959-25cb82edf23d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="Snow" style={{ width: '100 %' }} />
