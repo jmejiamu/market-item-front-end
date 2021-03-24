32c638099f0062232314d6c5d3568d39913adf74
Jose <jmejiamu@gmail.com>
2021-03-23 17:26:28 -0700

    added menu description items

--- src/components/Men/MenItems.js
+++ src/components/Men/MenItems.js
@@ -16,31 +16,31 @@ const MenItems = () => {
                     Mens Items
   </div>
                 <ul class="sidebar-navigation">
-                    <li class="header">Navigation</li>
+                    <li class="header">Men's clearance</li>
                     <li>
                         <a href="#">
-                            <i class="fa fa-home" aria-hidden="true"></i> Homepage
+                            <i class="fa fa-home" aria-hidden="true"></i> T-shirt
       </a>
                     </li>
                     <li>
                         <a href="#">
-                            <i class="fa fa-tachometer" aria-hidden="true"></i> Dashboard
+                            <i class="fa fa-tachometer" aria-hidden="true"></i> Jackets
       </a>
                     </li>
-                    <li class="header">Another Menu</li>
+                    <li class="header">Accesories</li>
                     <li>
                         <a href="#">
-                            <i class="fa fa-users" aria-hidden="true"></i> Friends
-      </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <i class="fa fa-cog" aria-hidden="true"></i> Settings
+                            <i class="fa fa-users" aria-hidden="true"></i> Wallet
       </a>
                     </li>
                     <li>
                         <a href="#">
-                            <i class="fa fa-info-circle" aria-hidden="true"></i> Information
+                            <i class="fa fa-cog" aria-hidden="true"></i> Hats
+      </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-info-circle" aria-hidden="true"></i> Watch
       </a>
                     </li>
                 </ul>
@@ -48,6 +48,19 @@ const MenItems = () => {
 
             <div class="content-container">
 
+                <div class="container-fluid">
+
+
+                    <div class="jumbotron">
+                        <h1>Navbar example</h1>
+                        <p>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
+                        <p>To see the difference between static and fixed top navbars, just scroll.</p>
+                        <p>
+                            <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">View navbar docs &raquo;</a>
+                        </p>
+                    </div>
+
+                </div>
                 <div class="container-fluid">
 
 
