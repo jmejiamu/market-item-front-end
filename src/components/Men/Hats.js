93aca4708b23497fe583dedaed1b708c0d039a06
Jose <jmejiamu@gmail.com>
2021-03-31 20:18:14 -0700

    added import in hat file

--- src/components/Men/Hats.js
+++ src/components/Men/Hats.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
+import { fetchData, hatSelector } from '../../features/fetchHats/fetchHats'
+
 const Hats = () => {
     return (
         <div>
