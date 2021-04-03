4d60e30cf51a368e7f4b74995c2b8d517d02c650
Jose <jmejiamu@gmail.com>
2021-04-02 23:01:23 -0700

    added fetch hats

--- src/components/Men/Hats.js
+++ src/components/Men/Hats.js
@@ -5,9 +5,33 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchData, hatSelector } from '../../features/fetchHats/fetchHats'
 
 const Hats = () => {
-    return (
-        <div>
 
+    const dispatch = useDispatch();
+    const { hats, loading, hasError } = useSelector(hatSelector);
+
+    // Error handling & map sucessfull query data
+    const renderData = () => {
+        if (loading) return <p>Loading...</p>
+        if (hasError) return <p>Something went wrong</p>
+
+        return hats.map((item, index) => {
+            return (
+                <div key={index} className='card card-style mt-5 grid-item mb-5'>
+                    <img className="card-img-top img-style-popular" src={item.itemUrl} alt="men" />
+                    <div className="card-body" >
+                        <h5 className="card-title">{item.itemName}</h5>
+                        <p className="card-text" >{item.itemDescription}</p>
+                    </div>
+                </div>
+            )
+        })
+    }
+    useEffect(() => {
+        dispatch(fetchData());
+    }, [dispatch])
+    return (
+        <div className="grid-container">
+            {renderData()}
         </div>
     );
 };
