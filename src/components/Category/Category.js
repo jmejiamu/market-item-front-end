4599a638dc83f4d098d78ed732cdc69dd3d9db84
Jose <jmejiamu@gmail.com>
2021-05-01 00:46:21 -0700

    added link to the electronic section

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -24,9 +24,11 @@ const Category = () => {
                         </Link>
                     </div>
                     <div class="column" >
-                        <img className="img-category" src="https://images.unsplash.com/photo-1557855506-3619a44bab73?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=701&q=80" alt="Snow" style={{ width: '100 %' }} />
+                        <Link>
+                            <img className="img-category" src="https://images.unsplash.com/photo-1557855506-3619a44bab73?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=701&q=80" alt="Snow" style={{ width: '100 %' }} />
 
-                        <div className="text" >Electronics</div>
+                            <div className="text" >Electronics</div>
+                        </Link>
 
                     </div>
                 </div>
