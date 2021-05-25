27c1148cb50442b7fcc5ee302ee6aba9063f5519
Jose <jmejiamu@gmail.com>
2021-05-24 21:30:01 -0700

    clean other components with card reusable component

--- src/components/Men/Watch.js
+++ src/components/Men/Watch.js
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { fetchData, watchSelector } from '../../features/fetchWatch/fetchWatch';
+import Card from '../itemsCard/Card';
 
 const Watch = () => {
 
@@ -16,13 +17,7 @@ const Watch = () => {
 
         return watch.map((item, index) => {
             return (
-                <div key={index} className='card card-style mt-5 grid-item mb-5'>
-                    <img className="card-img-top img-style-popular" src={item.itemUrl} alt="men" />
-                    <div className="card-body" >
-                        <h5 className="card-title">{item.itemName}</h5>
-                        <p className="card-text" >{item.itemDescription}</p>
-                    </div>
-                </div>
+                <Card item={item} />
             )
         })
     }
