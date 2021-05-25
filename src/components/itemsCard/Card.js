28fa2c64d9e643a23a055644888c989816ef70f4
Jose <jmejiamu@gmail.com>
2021-05-25 01:13:10 -0700

    added styles to the mens component

--- src/components/itemsCard/Card.js
+++ src/components/itemsCard/Card.js
@@ -2,11 +2,18 @@ import React from 'react'
 
 const Card = (props) => {
     return (
-        <div key={props.index} className='card card-style mt-5 grid-item mb-5'>
-            <img className="card-img-top img-style-popular" src={props.item.itemUrl} alt="men" />
-            <div className="card-body" >
-                <h5 className="card-title">{props.item.itemName}</h5>
-                <p className="card-text" >{props.item.itemDescription}</p>
+        <div key={props.index} className="max-w-sm space-y-2 rounded overflow-hidden shadow-lg mb-10 ">
+            <img class="w-full h-screen" src={props.item.itemUrl} alt="Sunset in the mountains" />
+            <div class="px-6 py-4">
+                <div class="font-bold text-xl mb-2">{props.item.itemName}</div>
+                <p class="text-gray-700 text-base">
+                    {props.item.itemDescription}
+                </p>
+            </div>
+            <div class="px-6 pt-4 pb-2">
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
             </div>
         </div>
     )
